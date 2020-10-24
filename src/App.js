70e40aa0899846ebf24cf56e95c3a84072f7bda9
Kelvin Brito <britok30@gmail.com>
2020-10-24 14:58:51 -0400

    about section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { AnimatedSwitch } from 'react-router-transition';
 import Hero from './components/Hero/Hero';
 import Projects from './components/Projects/Projects';
+import About from './components/About/About';
 
 function App() {
     return (
         <>
             <Router>
                 <Navbar />
-                <Switch>
+                <AnimatedSwitch
+                    atEnter={{ opacity: 0 }}
+                    atLeave={{ opacity: 0 }}
+                    atActive={{ opacity: 1 }}
+                    className="switch-wrapper"
+                >
                     <Route path="/" component={Hero} exact />
                     <Route path="/projects" component={Projects} exact />
-                </Switch>
+                    <Route path="/about" component={About} />
+                </AnimatedSwitch>
             </Router>
         </>
     );
---
+++ src/components/About/About.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import Fade from 'react-reveal';
+import './About.css';
+
+const About = () => {
+    return (
+        <div>
+            <div className="container about-container">
+                <div className="row about-row">
+                    <div className="col-md-7">
+                        <Fade bottom duration={3000} distance={'20px'}>
+                            <p className="about-p1">
+                                Liany Arvelo is a Interior Designer based in
+                                North Orlando.
+                            </p>
+                        </Fade>
+                    </div>
+
+                    <div className="col-md-5 col-container">
+                        <Fade bottom duration={3000} distance={'20px'}>
+                            <p className="about-p2">
+                                An early passion & appreciation for beautiful
+                                designs and exceptional home layouts is what
+                                inspired Liany to pursue a career in interior
+                                design. Being Dominican-American, she has a
+                                cultural background that has influenced her
+                                inspirations when it comes to interior design.
+                                After graduating from Seminole State College
+                                with her Bachelors Degree in Interior Design,
+                                she spent two years perfecting her craft at
+                                internships and now currently works for a design
+                                company based in Orlando, FL.
+                                <br />
+                                <br />
+                                She offers tailor-made solutions for the layout
+                                and decoration of your interior. Each project is
+                                born from the meeting between a person and a
+                                place, its history, and the desire to project
+                                there to live there. Pleasant spaces in which we
+                                want to build memories
+                            </p>
+                        </Fade>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
