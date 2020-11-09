989cb6220e0bfd98fd60a06df4195fa25dc4c73b
Kelvin Brito <britok30@gmail.com>
2020-11-08 21:05:00 -0500

    updates

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Fade } from 'react-reveal';
+import BackgroundSlider from 'react-background-slider';
+import image1 from '../../images/hero.jpg';
+import image2 from '../../images/jonathan-borba-eyZwrNbaCto-unsplash.jpg';
 import './Hero.css';
 
 const Hero = () => {
-    const text = 'Bring beauty to your space';
+    const text = 'Arvelo Design: Modern Inspiration';
 
     return (
         <>
-            <div className="hero-container">
+            {/* <div className="hero-container">
                 <div className="inner-container">
                     <Fade bottom duration={3000}>
                         <h1 className="hero-header">{text.toUpperCase()}</h1>
                     </Fade>
                 </div>
-            </div>
+            </div> */}
+            <BackgroundSlider
+                images={[image1, image2]}
+                duration={10}
+                transition={2}
+            />
+            {/* <h1 className="hero-header">{text}</h1> */}
         </>
     );
 };
